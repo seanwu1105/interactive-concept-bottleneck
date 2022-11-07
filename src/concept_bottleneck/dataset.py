cf2869c500f1b259cb76b99e55fde8e6a893e052
Shuang Wu <seanwu1105@gmail.com>
2022-11-07 14:16:33 -0500

    feat: add dataset to load attributes and classes

--- src/concept_bottleneck/dataset.py
+++ src/concept_bottleneck/dataset.py
@@ -12,17 +12,16 @@ TransformedT = typing.TypeVar("TransformedT")
 TargetTransformedT = typing.TypeVar("TargetTransformedT")
 
 
-class CUB200_2011(
-    Dataset[tuple[TransformedT, TargetTransformedT]]
-):  # pylint: disable=invalid-name
-    url = "https://data.caltech.edu/records/65de6-vp158/files/CUB_200_2011.tgz"
-    md5 = "97eceeb196236b17998738112f37df78"
-    root = pathlib.Path(__file__).parent.resolve() / "data"
+URL = "https://data.caltech.edu/records/65de6-vp158/files/CUB_200_2011.tgz"
+MD5 = "97eceeb196236b17998738112f37df78"
+ROOT = pathlib.Path(__file__).parent.resolve() / "data"
 
+
+class CUB200ImageToAttributes(Dataset[tuple[TransformedT, TargetTransformedT]]):
     def __init__(
         self,
         train: bool,
-        download: bool = True,
+        download: bool = False,
         transform: typing.Callable[[Image.Image], TransformedT] = lambda x: x,
         target_transform: typing.Callable[
             [npt.NDArray[np.int_]], TargetTransformedT
@@ -33,29 +32,25 @@ class CUB200_2011(
         self._target_transform = target_transform
 
         if download:
-            download_and_extract_archive(
-                url=self.url, download_root=str(self.root), md5=self.md5
-            )
+            download_and_extract_archive(url=URL, download_root=str(ROOT), md5=MD5)
 
         self._train_test_split = self._load_train_test_split()
         self._attributes = self._load_image_attribute_labels(train)
         self._image_paths = self._load_image_paths()
 
     def _load_train_test_split(self):
-        filepath = self.root / "CUB_200_2011" / "train_test_split.txt"
+        filepath = ROOT / "CUB_200_2011" / "train_test_split.txt"
         return np.loadtxt(filepath, usecols=1, dtype=np.int_)
 
     def _load_image_attribute_labels(self, train: bool) -> npt.NDArray[np.int_]:
-        filepath = (
-            self.root / "CUB_200_2011" / "attributes" / "image_attribute_labels.txt"
-        )
+        filepath = ROOT / "CUB_200_2011" / "attributes" / "image_attribute_labels.txt"
 
         attributes = np.loadtxt(filepath, usecols=(0, 2), dtype=np.int_)
         grouped = groupby(attributes, col=0)
         return grouped[np.nonzero(self._train_test_split == train)]
 
     def _load_image_paths(self):
-        filepath = self.root / "CUB_200_2011" / "images.txt"
+        filepath = ROOT / "CUB_200_2011" / "images.txt"
         with open(filepath, encoding="utf-8") as f:
             image_paths = tuple(line.split()[1] for line in f.readlines())
         return image_paths
@@ -64,10 +59,60 @@ class CUB200_2011(
         return len(self._attributes)
 
     def __getitem__(self, idx: int) -> tuple[TransformedT, TargetTransformedT]:
-        image_path = self.root / "CUB_200_2011" / "images" / self._image_paths[idx]
+        image_path = ROOT / "CUB_200_2011" / "images" / self._image_paths[idx]
         return (
             self._transform(pil_loader(str(image_path))),
             self._target_transform(self._attributes[idx]),
+        )
+
+    @property
+    def num_attributes(self) -> int:
+        return self._attributes.shape[1]
+
+
+class CUB200AttributesToClass(Dataset[tuple[TransformedT, TargetTransformedT]]):
+    def __init__(
+        self,
+        train: bool,
+        download: bool = False,
+        transform: typing.Callable[[npt.NDArray[np.int_]], TransformedT] = lambda x: x,
+        target_transform: typing.Callable[
+            [npt.NDArray[np.int_]], TargetTransformedT
+        ] = lambda x: x,
+    ):
+        super().__init__()
+        self._transform = transform
+        self._target_transform = target_transform
+
+        if download:
+            download_and_extract_archive(url=URL, download_root=str(ROOT), md5=MD5)
+
+        self._train_test_split = self._load_train_test_split()
+        self._attributes = self._load_image_attribute_labels(train)
+        self._classes = self._load_classes()
+
+    def _load_train_test_split(self):
+        filepath = ROOT / "CUB_200_2011" / "train_test_split.txt"
+        return np.loadtxt(filepath, usecols=1, dtype=np.int_)
+
+    def _load_image_attribute_labels(self, train: bool) -> npt.NDArray[np.int_]:
+        filepath = ROOT / "CUB_200_2011" / "attributes" / "image_attribute_labels.txt"
+
+        attributes = np.loadtxt(filepath, usecols=(0, 2), dtype=np.int_)
+        grouped = groupby(attributes, col=0)
+        return grouped[np.nonzero(self._train_test_split == train)]
+
+    def _load_classes(self):
+        filepath = ROOT / "CUB_200_2011" / "image_class_labels.txt"
+        return np.loadtxt(filepath, usecols=1, dtype=np.int_)
+
+    def __len__(self):
+        return len(self._attributes)
+
+    def __getitem__(self, idx: int) -> tuple[TransformedT, TargetTransformedT]:
+        return (
+            self._transform(self._attributes[idx]),
+            self._target_transform(self._classes[idx]),
         )
 
     @property
--- tests/concept_bottleneck/test_dataset.py
+++ tests/concept_bottleneck/test_dataset.py
@@ -2,24 +2,20 @@ import random
 
 import numpy as np
 
-from src.concept_bottleneck.dataset import CUB200_2011
+from src.concept_bottleneck.dataset import ROOT, CUB200ImageToAttributes
 
-CUB200_2011(train=True, download=True)
+CUB200ImageToAttributes(train=True, download=True)
 
 
 def test_cub200_2011_attributes_image_ids_are_sorted():
-    filepath = (
-        CUB200_2011.root / "CUB_200_2011" / "attributes" / "image_attribute_labels.txt"
-    )
+    filepath = ROOT / "CUB_200_2011" / "attributes" / "image_attribute_labels.txt"
 
     attributes = np.loadtxt(filepath, usecols=0, dtype=np.int_)
     assert np.all(np.diff(attributes) >= 0)
 
 
 def test_cub200_2011_attributes_ids_are_sorted():
-    filepath = (
-        CUB200_2011.root / "CUB_200_2011" / "attributes" / "image_attribute_labels.txt"
-    )
+    filepath = ROOT / "CUB_200_2011" / "attributes" / "image_attribute_labels.txt"
 
     attributes = np.loadtxt(filepath, usecols=(0, 1), dtype=np.int_)
     image_ids = np.unique(attributes[:, 0])
@@ -31,23 +27,29 @@ def test_cub200_2011_attributes_ids_are_sorted():
 
 
 def test_cub200_2011_train_test_split_image_ids_are_sorted():
-    filepath = CUB200_2011.root / "CUB_200_2011" / "train_test_split.txt"
+    filepath = ROOT / "CUB_200_2011" / "train_test_split.txt"
     train_test_split = np.loadtxt(filepath, usecols=0, dtype=np.int_)
     assert np.all(np.diff(train_test_split) >= 0)
 
 
+def test_cub200_2011_image_class_labels_image_ids_are_sorted():
+    filepath = ROOT / "CUB_200_2011" / "image_class_labels.txt"
+    image_class_labels = np.loadtxt(filepath, usecols=0, dtype=np.int_)
+    assert np.all(np.diff(image_class_labels) >= 0)
+
+
 def test_cub200_2011_train_size():
-    dataset = CUB200_2011(train=True, download=False)
+    dataset = CUB200ImageToAttributes(train=True)
     assert len(dataset) == 5994
 
 
 def test_cube200_2011_test_size():
-    dataset = CUB200_2011(train=False, download=False)
+    dataset = CUB200ImageToAttributes(train=False)
     assert len(dataset) == 5794
 
 
 def test_cube200_2011_getitem():
-    dataset = CUB200_2011(train=True, download=False)
+    dataset = CUB200ImageToAttributes(train=True)
 
     for _ in range(100):
         idx = random.randint(0, len(dataset) - 1)
