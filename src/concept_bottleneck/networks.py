26c23539670c9322fdbab026cdb15645e62882ff
Shuang Wu <seanwu1105@gmail.com>
2022-11-16 23:17:19 -0500

    feat: use pretrained weights for inception model

--- src/concept_bottleneck/networks.py
+++ src/concept_bottleneck/networks.py
@@ -5,12 +5,16 @@ from src.concept_bottleneck.dataset import NUM_ATTRIBUTES, NUM_CLASSES
 
 
 def get_inception() -> torch.nn.Module:
-    return torch.hub.load(
+    model = torch.hub.load(
         "pytorch/vision:v0.10.0",
         "inception_v3",
-        init_weights=False,
-        num_classes=NUM_ATTRIBUTES,
+        weights="IMAGENET1K_V1",
     )
+
+    model.AuxLogits.fc = torch.nn.Linear(in_features=768, out_features=NUM_ATTRIBUTES)
+    model.fc = torch.nn.Linear(in_features=2048, out_features=NUM_ATTRIBUTES)
+
+    return model
 
 
 def get_mlp() -> torch.nn.Module:
